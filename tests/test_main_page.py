b95329b48fcf5d9bdbc4d3c52dcd37c6878c09c6
Pavel Chernenko <chernenkopavel63@gmail.com>
2023-11-02 20:33:30 +0100

    upgrade

---
+++ tests/test_main_page.py
@@ -0,0 +1,21 @@
+import pytest
+# from pages.main_page import MainPage
+from pages.base_page import BasePage
+from settings import sets
+
+
+
+@pytest.mark.smoke
+@pytest.mark.regression
+@pytest.mark.main_page
+class TestMainPage
+
+    def setup_method(self):
+        pass
+
+    def test_get_main_page(self, browser):
+        page = BasePage(browser, sets.PROD_SERVER)
+        page.open()
+
+    # def test_login_logout(self, browser):
+    #     link_to_site = browser.current_url
