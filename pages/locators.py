fb48caafc7c86fced06b3f130728934485ae76a2
Pavel Chernenko <chernenkopavel63@gmail.com>
2023-11-05 16:39:43 +0100

    Upgrade data locators

--- pages/locators.py
+++ pages/locators.py
@@ -1,19 +1,28 @@
+from selenium.webdriver.common.by import By
 
-XPATH кнопки "Хиты"
-    //a[@class='check-item check-hot']
-XPATH кнопки "Скидки"
-    //a[@class='check-item check-sale']
-XPATH кнопки "Новинки"
-    //a[@class='check-item']
-XPATH категории SAMSUNG
-    SAMSUNG = (By.XPATH, "//div[text()='Samsung']")
-XPATH выбора модели SAMSUNG-J701
-    //a[@href='BrandModel/Samsung-J701']
 
-#Футтер
-XPATH кнопки "Подписаться"
-    //button[@class='newsletter_button']
-XPATH поля ввода E-mail для подписки
-    //input[@class='newsletter_input']
-XPATH логотипа "CaSeNik"
-    //img[@src='images/logo-footer.png']
+LOGIN_SINGUP = (By.XPATH, "//div[@class='top_bar_user']/a[@href='user/login']")
+FEEDBACK = (By.XPATH, "//a[text()='Обратная связь']")
+LOGISTIC = (By.XPATH, "//a[text()='Доставка']")
+WARRANTY = (By.XPATH, "//a[text()='Гарантия']")
+PHONE = (By.XPATH, "//*[text()='+38 098 911 95 22']")
+CURRENCY = (By.XPATH, "//select[@id='currency']")
+CURRENCY_UAH = (By.XPATH, "//select[@id='currency']/option[text()='UAH']")
+CURRENCY_USD = (By.XPATH, "//select[@id='currency']/option[text()='USD']")
+CURRENCY_EUR = (By.XPATH, "//select[@id='currency']/option[text()='EUR']")
+LOGO_DESK = (By.XPATH, "//div[@class='logo logo-desk']/a/ing")
+LOGO_MOB = (By.XPATH, "//div[@class='logo logo-mob']/a/ing")
+SEARCH = (By.XPATH, "//*[@id='typehead']")
+SUBMIT = (By.XPATH, "//div[@class='header_search']//button")
+WISH = (By.XPATH, "//div[contains(@class, 'wishlist')]/div[@class='wishlist_icon']/a/img")
+CART = (By.XPATH, "//div[@class='cart']//div[@class='cart_icon']/a/img")
+HITS = (By.XPATH, "//a[@class='check-item check-hot']")
+DISCOUNTS = (By.XPATH, "//a[@class='check-item check-sale']")
+NEW_ITEMS = (By.XPATH, "//a[@class='check-item']")
+SAMSUNG_CAT = (By.XPATH, "//div[text()='Samsung']")
+SAMSUNG_J701 = (By.XPATH, "//a[@href='BrandModel/Samsung-J701']")
+SUBSCRUBE = (By.XPATH, "//button[@class='newsletter_button']")
+INPUT_SUBSCRIBE = (By.XPATH,"//input[@class='newsletter_input']")
+LOGO_FOOT = (By.XPATH, "//img[@src='images/logo-footer.png']")
+
+
