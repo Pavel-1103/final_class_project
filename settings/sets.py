96ad3d09e9d4762b041e49853d4cc4093739a85b
Pavel Chernenko <chernenkopavel63@gmail.com>
2023-11-06 12:01:59 +0100

    upgrade data

---
+++ settings/sets.py
@@ -0,0 +1,6 @@
+PROD_SERWER = "https://casenik.com.ua/"
+TEST_EMAIL = "wedoko7133@bookspre.com"
+PASSWORD = "Qwerty123456"
+IMPLICITLY_WAIT = 15 # Неявные ожидания
+EXPLICIT_WAIT = 15 # Явные ожидания
+
