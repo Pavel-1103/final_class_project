f4f73e6720819fd3f2667d5280f442ba58469507
Pavel Chernenko <chernenkopavel63@gmail.com>
2023-11-07 12:31:47 +0100

    Update data in locators

--- pages/locators.py
+++ pages/locators.py
@@ -2,11 +2,13 @@ from selenium.webdriver.common.by import By
 
 class BasePageLocators:
     LOGIN_SINGUP = (By.XPATH, "//div[@class='top_bar_user']/a[@href='user/login']")
+    DETAILS = (By.XPATH, "//a[text() = 'Детали сотрудничества']")
     FEEDBACK = (By.XPATH, "//a[text()='Обратная связь']")
     LOGISTIC = (By.XPATH, "//a[text()='Доставка']")
     WARRANTY = (By.XPATH, "//a[text()='Гарантия']")
     PHONE = (By.XPATH, "//*[text()='+38 098 911 95 22']")
     CURRENCY = (By.XPATH, "//select[@id='currency']")
+
     CURRENCY_UAH = (By.XPATH, "//select[@id='currency']/option[text()='UAH']")
     CURRENCY_USD = (By.XPATH, "//select[@id='currency']/option[text()='USD']")
     CURRENCY_EUR = (By.XPATH, "//select[@id='currency']/option[text()='EUR']")
@@ -21,6 +23,7 @@ class BasePageLocators:
     NEW_ITEMS = (By.XPATH, "//a[@class='check-item']")
     SAMSUNG_CAT = (By.XPATH, "//div[text()='Samsung']")
     SAMSUNG_J701 = (By.XPATH, "//a[@href='BrandModel/Samsung-J701']")
+
     SUBSCRUBE = (By.XPATH, "//button[@class='newsletter_button']")
     INPUT_SUBSCRIBE = (By.XPATH,"//input[@class='newsletter_input']")
     LOGO_FOOT = (By.XPATH, "//img[@src='images/logo-footer.png']")
